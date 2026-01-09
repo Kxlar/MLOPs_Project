ec0e1262faad46aaa55434f4993d498caa0fc920
KOLAR Pierre-Eduard <s254145@dtu.dk>
2026-01-09 12:48:23 +0100

    First commit + cookiecutter template

---
+++ src/anomaly_detection/train.py
@@ -0,0 +1,10 @@
+from anomaly_detection.model import Model
+from anomaly_detection.data import MyDataset
+
+def train():
+    dataset = MyDataset("data/raw")
+    model = Model()
+    # add rest of your training code here
+
+if __name__ == "__main__":
+    train()
